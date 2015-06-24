7cf5c822ce7e2dcf161ada6473dccf2842204630
ChunEon Park <hermet@hermet.pe.kr>
2015-06-24 15:59:45 +0900

    Revert "eo: sync with eolian syntax changes."
    This reverts commit a81a0d23ee29f37338e142de6acc755a1542fd97.

--- src/lib/enventor_smart.c
+++ src/lib/enventor_smart.c
@@ -223,16 +223,14 @@ _enventor_object_evas_object_smart_clip_unset(Evas_Object *obj EINA_UNUSED, Enve
    evas_object_clip_unset(o);
 }
 
-EOLIAN static Eo *
+EOLIAN static void
 _enventor_object_eo_base_constructor(Eo *obj,
                                      Enventor_Object_Data *pd EINA_UNUSED)
 {
-   obj = eo_do_super_ret(obj, MY_CLASS, obj, eo_constructor());
+   eo_do_super(obj, MY_CLASS, eo_constructor());
    eo_do(obj,
          evas_obj_type_set(MY_CLASS_NAME_LEGACY),
          evas_obj_smart_callbacks_descriptions_set(_smart_callbacks));
-
-   return obj;
 }
 
 EOLIAN static Eina_Bool
